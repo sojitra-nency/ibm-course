a837be257784ae8054dd9c88bb1249a521846421
SOJITRA NENCY <78294964+sojitra-nency@users.noreply.github.com>
2023-02-09 14:59:22 +0530

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,5 +1,14 @@
-echo "enter p,r,n values";
-read p,r,n;
-$a = expr `p*r*n/100`;
-echo $a
-echo "second pull request"
+#!/bin/bash
+
+echo "Enter the Principal amount: "
+read principal
+
+echo "Enter the Rate of interest: "
+read rate
+
+echo "Enter the Time period (in years): "
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc -l)
+
+echo "The Simple Interest is: $interest"
